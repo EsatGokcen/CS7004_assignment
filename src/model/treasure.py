c8874d9c0173d02a2355d3e5afbf798a4f25505f
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-08 18:43:10 +0100

    updated treasure class to use treasure type enum class

--- src/model/treasure.py
+++ src/model/treasure.py
@@ -1,14 +1,30 @@
-class Treasure:
+from src.model.treasure_type import TreasureType
 
-    def __init__(self, treasure_type: str):
-        self.__value: float = 100.00
+class Treasure:
+    def __init__(self, treasure_type: TreasureType):
         self.__type = treasure_type
+        self.__value: float = treasure_type.base_value
+        self.__collected: bool = False
 
     def get_value(self) -> float:
         return self.__value
 
-    def get_type(self) -> str:
+    def get_type(self) -> TreasureType:
         return self.__type
 
+    def is_depleted(self) -> bool:
+        return self.__value <= 0
+
+    def mark_collected(self):
+        self.__collected = True
+
+    def is_collected(self) -> bool:
+        return self.__collected
+
     def lose_value(self) -> None:
-        self.__value = self.__value * 0.09
+        self.__value -= self.__value * 0.001  # Lose 0.1%
+        if self.__value < 0:
+            self.__value = 0
+
+    def get_wealth_increase(self) -> float:
+        return self.__type.wealth_increase
