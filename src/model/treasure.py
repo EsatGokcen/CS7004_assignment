5f7aa19ee2bb03fc30a3f0b010ff4b0a8a68aba5
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-10 15:22:48 +0100

    used unittest to test treasure class with decay and depletion

--- src/model/treasure.py
+++ src/model/treasure.py
@@ -22,8 +22,8 @@ class Treasure:
         return self.__collected
 
     def lose_value(self) -> None:
-        self.__value -= self.__value * 0.001  # Lose 0.1%
-        if self.__value < 0:
+        self.__value -= self.__value * 0.01  # Lose 0.1%
+        if self.__value < 0.001:
             self.__value = 0
 
     def get_wealth_increase(self) -> float:
---
+++ src/tests/test_treasure.py
@@ -0,0 +1,25 @@
+import unittest
+from src.model.treasure import Treasure, TreasureType
+
+class TestTreasure(unittest.TestCase):
+
+    def test_initial_value_and_type(self):
+        treasure = Treasure(TreasureType.SILVER)
+        self.assertEqual(treasure.get_type(), TreasureType.SILVER)
+        self.assertEqual(treasure.get_value(), TreasureType.SILVER.base_value)
+
+    def test_treasure_decay(self):
+        treasure = Treasure(TreasureType.BRONZE)
+        initial_value = treasure.get_value()
+        treasure.lose_value()
+        self.assertLess(treasure.get_value(), initial_value)
+
+    def test_treasure_depletion(self):
+        treasure = Treasure(TreasureType.BRONZE)
+        for _ in range(2000):
+            treasure.lose_value()
+        self.assertTrue(treasure.is_depleted())
+        self.assertAlmostEqual(treasure.get_value(), 0.0, places=6)
+
+if __name__ == '__main__':
+    unittest.main()
