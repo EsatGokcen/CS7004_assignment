c2b4bef777a7cad97b0d75b828112d8c601e8073
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-13 20:26:12 +0100

    connected skill class to hunter and added getter methods

--- src/model/treasure_hunter.py
+++ src/model/treasure_hunter.py
@@ -1,9 +1,10 @@
 from src.model.cell import Cell
+from src.model.hunter_skills import Skill
 
 
 class TreasureHunter:
 
-    def __init__(self, cell: Cell, skill):
+    def __init__(self, cell: Cell, skill: Skill):
         self._cell = cell
         self._stamina: float = 100.0
         self._carried_treasure = None
@@ -11,3 +12,15 @@ class TreasureHunter:
         self._memory = {"treasures": [], "hideouts": []}
         self._survival_steps_remaining = 3
         cell.add_object(self)
+
+    def get_cell(self) -> Cell:
+        return self._cell
+
+    def get_stamina(self) -> float:
+        return self._stamina
+
+    def get_skill(self) -> Skill:
+        return self._skill
+
+    def get_memory(self) -> dict:
+        return self._memory
