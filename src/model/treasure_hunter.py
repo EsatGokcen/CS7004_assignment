72f4846e9b63867a7c505a240873f915eeaa9810
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-13 20:31:09 +0100

    added stamina and survival related methods to hunter class

--- src/model/treasure_hunter.py
+++ src/model/treasure_hunter.py
@@ -38,3 +38,16 @@ class TreasureHunter:
             self._stamina += 1.0
             if self._stamina > 100.0:
                 self._stamina = 100.0
+
+    def is_critical(self) -> bool:
+        return self._stamina <= 6.0
+
+    def is_collapsed(self) -> bool:
+        return self._stamina <= 0.0
+
+    def tick_survival(self) -> None:
+        if self.is_collapsed():
+            self._survival_steps_remaining -= 1
+
+    def can_act(self) -> bool:
+        return not self.is_collapsed() or self._survival_steps_remaining > 0
