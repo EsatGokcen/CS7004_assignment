922c30e79a8077d4fe45ad69ecfb4f156ce8c8e0
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-14 20:34:32 +0100

    tested initial stamina and state and move to method for hunter

--- src/tests/test_hunter.py
+++ src/tests/test_hunter.py
@@ -14,5 +14,16 @@ class TestTreasureHunter(unittest.TestCase):
         self.treasure = Treasure(TreasureType.SILVER)
         self.hideout_cell = Cell(1, 1)
 
+    def test_initial_stamina_and_state(self):
+        self.assertEqual(self.hunter.get_stamina(), 100.0)
+        self.assertFalse(self.hunter.is_carrying_treasure())
+        self.assertEqual(self.hunter.get_memory(), {'treasures': [], 'hideouts': []})
+
+    def test_move_to_reduces_stamina(self):
+        new_cell = Cell(0, 1)
+        self.hunter.move_to(new_cell)
+        self.assertEqual(self.hunter.get_stamina(), 98.0)
+        self.assertEqual(self.hunter.get_cell(), new_cell)
+
 if __name__ == '__main__':
     unittest.main()
