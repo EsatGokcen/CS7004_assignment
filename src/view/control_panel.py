adb48bc16aca63d3acb2af52cbe68b78e11c8eb7
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-15 21:48:08 +0100

    added control panel class with tkinter

---
+++ src/view/control_panel.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+class Controls:
+    def __init__(self, master: tk.Widget):
+        self.frame = tk.Frame(master, padx=10, pady=10)
+
+        self.start_button = tk.Button(self.frame, text="Start", command=self.start)
+        self.pause_button = tk.Button(self.frame, text="Pause", command=self.pause)
+        self.reset_button = tk.Button(self.frame, text="Reset", command=self.reset)
+
+        self.start_button.pack(fill="x")
+        self.pause_button.pack(fill="x", pady=5)
+        self.reset_button.pack(fill="x")
+
+        # Placeholder callbacks, to be set from main_window
+        self.on_start = lambda: None
+        self.on_pause = lambda: None
+        self.on_reset = lambda: None
+
+    def start(self):
+        self.on_start()
+
+    def pause(self):
+        self.on_pause()
+
+    def reset(self):
+        self.on_reset()
