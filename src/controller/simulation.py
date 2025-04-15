a87ca5d184a9d6bd58e70993ad8e08cb7719f4d9
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-15 17:53:22 +0100

    added hunter and hideout generation logic to simulation class

--- src/controller/simulation.py
+++ src/controller/simulation.py
@@ -2,6 +2,9 @@ from src.controller.config import *
 from src.model.eldoria_map import EldoriaMap
 from src.model.treasure_type import TreasureType
 from src.model.treasure import Treasure
+from src.model.treasure_hunter import TreasureHunter
+from src.model.hideout import Hideout
+from src.model.hunter_skills import Skill
 from random import randint, choice
 
 class Simulation:
@@ -9,6 +12,7 @@ class Simulation:
     def __init__(self, map_obj: EldoriaMap):
         self.map = map_obj
         self.__running = False
+        self._hideouts = []
 
     def scatter_treasures(self, num_treasures: int) -> None:
         for _ in range(num_treasures):
@@ -27,6 +31,21 @@ class Simulation:
                         obj.lose_value()
                         if obj.is_depleted():
                             cell.remove_object(obj)
+
+    def place_hideouts_and_hunters(self, num_hideouts: int, hunters_per_hideout_range: tuple) -> None:
+
+        for _ in range(num_hideouts):
+            x = randint(0, self.map.get_width() - 1)
+            y = randint(0, self.map.get_height() - 1)
+            cell = self.map.get_cell(x, y)
+            hideout = Hideout(cell)
+            self._hideouts.append(hideout)
+
+            num_hunters = randint(*hunters_per_hideout_range)
+            for _ in range(num_hunters):
+                skill = choice(list(Skill))
+                hunter = TreasureHunter(cell, skill)
+                hideout.add_hunter(hunter)
 
     def run(self):
         self.scatter_treasures(NUM_INITIAL_TREASURES)
