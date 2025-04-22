5be7b201ba7ef7e5af791eba2ca2374b7170b4c9
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-22 15:45:18 +0100

    add and remove knight test methods for garrison unittest

--- src/tests/test_garrison.py
+++ src/tests/test_garrison.py
@@ -1,5 +1,6 @@
 import unittest
 from src.model.garrison import Garrison
+from src.model.knight import Knight
 from src.model.cell import Cell
 
 class TestGarrison(unittest.TestCase):
@@ -11,3 +12,16 @@ class TestGarrison(unittest.TestCase):
     def test_garrison_initially_empty(self):
         self.assertEqual(len(self.garrison.get_knights()), 0)
         self.assertTrue(self.garrison.has_space())
+
+    def test_add_knight(self):
+        knight = Knight(self.cell)
+        self.garrison.add_knight(knight)
+        self.assertIn(knight, self.garrison.get_knights())
+        self.assertIn(knight, self.cell.get_contents())
+
+    def test_remove_knight(self):
+        knight = Knight(self.cell)
+        self.garrison.add_knight(knight)
+        self.garrison.remove_knight(knight)
+        self.assertNotIn(knight, self.garrison.get_knights())
+        self.assertNotIn(knight, self.cell.get_contents())
