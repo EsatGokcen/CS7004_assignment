33a2c1904cfa6ffbf6f0361a270167115c34b97c
Esat Gokcen <esatgokcen173@gmail.com>
2025-04-22 15:19:45 +0100

    initial test knight file with mock hunter and knight unittest classes

---
+++ src/tests/test_knight.py
@@ -0,0 +1,20 @@
+import unittest
+from src.model.knight import Knight
+from src.model.eldoria_map import EldoriaMap
+from src.model.treasure_hunter import TreasureHunter
+from src.model.hunter_skills import Skill
+
+class MockTreasureHunter(TreasureHunter):
+    def __init__(self, cell, wealth=10):
+        super().__init__(cell, Skill.NAVIGATION)
+        self._wealth = wealth
+
+    def modify_stamina(self, num):
+        self._stamina += num
+
+class TestKnight(unittest.TestCase):
+
+    def setUp(self):
+        self.map = EldoriaMap(10, 10)
+        self.start_cell = self.map.get_cell(5, 5)
+        self.knight = Knight(self.start_cell)
