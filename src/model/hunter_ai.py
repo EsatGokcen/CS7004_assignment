d1737dfacf7a8dc123b7382bf36524011a3152fc
Esat Gokcen <esatgokcen173@gmail.com>
2025-05-04 18:37:18 +0100

    improved hunter training data in sklearn, got better results

--- src/model/hunter_ai.py
+++ src/model/hunter_ai.py
@@ -1,24 +1,42 @@
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 
-# Simulated training data: [stamina, dist_to_treasure, dist_to_hideout, knight_nearby (1/0)]
+# Better balanced training data:
+# [stamina, dist_to_treasure, dist_to_hideout, knight_nearby (1/0)]
 X = np.array([
-    [90, 2, 5, 0],  # go for treasure
-    [40, 2, 1, 1],  # return to hideout
+    [95, 1, 5, 0],  # go to treasure
+    [90, 2, 4, 0],  # go to treasure
+    [80, 1, 6, 1],  # go to treasure
+    [70, 2, 3, 1],  # go to treasure
+    [60, 4, 2, 1],  # return to hideout
+    [50, 3, 1, 1],  # return to hideout
+    [40, 2, 1, 0],  # return to hideout
+    [30, 2, 2, 0],  # return to hideout
+    [20, 3, 1, 0],  # rest
     [10, 5, 1, 0],  # rest
-    [70, 1, 4, 1],  # avoid knight
-    [95, 1, 5, 0],  # go for treasure
-    [15, 4, 2, 1],  # rest
-    [30, 1, 1, 1],  # return
+    [15, 1, 2, 1],  # rest
+    [85, 1, 6, 1],  # go to treasure
+    [45, 3, 2, 0],  # return to hideout
+    [25, 2, 2, 1],  # avoid knight
+    [35, 3, 3, 1],  # avoid knight
+    [90, 1, 4, 1],  # go to treasure
+    [70, 1, 3, 0],  # go to treasure
+    [15, 2, 1, 1],  # rest
+    [55, 2, 4, 1],  # return
+    [65, 1, 4, 1],  # go to treasure
 ])
 
-# Labels: 0 = rest, 1 = go to treasure, 2 = return to hideout, 3 = avoid knight
-y = np.array([1, 2, 0, 3, 1, 0, 2])
+y = np.array([
+    1, 1, 1, 1,
+    2, 2, 2, 2,
+    0, 0, 0,
+    1, 2, 3, 3,
+    1, 1, 0, 2, 1
+])
 
 model = DecisionTreeClassifier()
 model.fit(X, y)
 
-# Decision function to use in-game
 def decide_action(stamina, dist_to_treasure, dist_to_hideout, knight_nearby):
     input_data = np.array([[stamina, dist_to_treasure, dist_to_hideout, knight_nearby]])
     return model.predict(input_data)[0]
