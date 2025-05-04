e99fafe1e5ce5520447dbe54269de565728e5841
Esat Gokcen <esatgokcen173@gmail.com>
2025-05-04 18:29:17 +0100

    updated hunter logic to use sklearn training data to make smarter decisions

--- src/controller/hunter_and_hideout_controller.py
+++ src/controller/hunter_and_hideout_controller.py
@@ -4,6 +4,7 @@ from src.model.treasure_hunter import TreasureHunter
 from src.model.hunter_skills import Skill
 from src.model.treasure import Treasure
 from src.model.eldoria_map import EldoriaMap
+from src.model.hunter_ai import decide_action
 from random import randint, choice
 from typing import List
 
@@ -35,7 +36,6 @@ def step_hunters(hideouts: List[Hideout], map_obj: EldoriaMap) -> None:
             if not hunter.can_act():
                 continue
 
-            # Scan visible treasure and update memory
             found_positions = hunter.scan_for_treasure(map_obj)
             found_treasures = []
             for pos in found_positions:
@@ -46,36 +46,36 @@ def step_hunters(hideouts: List[Hideout], map_obj: EldoriaMap) -> None:
 
             hunter.remember(found_treasures, [h.get_cell() for h in hideouts])
 
-            # Handle resting
-            if hunter.is_critical():
-                known_hideouts = hunter.get_memory()["hideouts"]
-                if known_hideouts:
-                    closest = min(known_hideouts, key=lambda h: map_obj.get_distance(hunter.get_cell(), h))
-                    neighbors = map_obj.get_adjacent_cells(hunter.get_cell())
-                    next_step = min(neighbors, key=lambda c: map_obj.get_distance(c, closest))
-                    hunter.move_to(next_step)
-                else:
-                    hunter.rest()
-                continue
+            stamina = hunter.get_stamina()
+            known_hideouts = hunter.get_memory()["hideouts"]
+            known_treasures = hunter.get_memory()["treasures"]
+            dist_to_hideout = (
+                min(map_obj.get_distance(hunter.get_cell(), h) for h in known_hideouts)
+                if known_hideouts else 10
+            )
+            dist_to_treasure = (
+                min(map_obj.get_distance(hunter.get_cell(), find_cell_for_treasure(map_obj, t) or hunter.get_cell())
+                    for t in known_treasures)
+                if known_treasures else 10
+            )
 
+            knight_nearby = 0
+            for neighbor in map_obj.get_adjacent_cells(hunter.get_cell()):
+                if any(obj.__class__.__name__ == "Knight" for obj in neighbor.get_contents()):
+                    knight_nearby = 1
+                    break
+
+            action = decide_action(stamina, dist_to_treasure, dist_to_hideout, knight_nearby)
             cell = hunter.get_cell()
             neighbors = map_obj.get_adjacent_cells(cell)
             visible_objects = [obj for neighbor in neighbors for obj in neighbor.get_contents()]
 
             treasures = [obj for obj in visible_objects if isinstance(obj, Treasure)]
-            unused_hideouts = [obj for obj in visible_objects if isinstance(obj, Hideout)]
 
-            if hunter.is_carrying_treasure():
-                for neighbor in neighbors:
-                    if any(isinstance(obj, Hideout) for obj in neighbor.get_contents()):
-                        hunter.move_to(neighbor)
-                        for obj in neighbor.get_contents():
-                            if isinstance(obj, Hideout):
-                                obj.store_treasure(hunter)
-                        break
-                else:
-                    hunter.move_to(choice(neighbors))
-            else:
+            if action == 0:
+                hunter.rest()
+
+            elif action == 1 and not hunter.is_carrying_treasure():
                 if treasures:
                     best = max(treasures, key=lambda t: t.get_value())
                     for neighbor in neighbors:
@@ -84,27 +84,50 @@ def step_hunters(hideouts: List[Hideout], map_obj: EldoriaMap) -> None:
                             hunter._carried_treasure = best
                             best.mark_collected()
                             break
-                else:
-                    remembered = hunter.get_memory()["treasures"]
-                    if remembered:
-                        closest = min(
-                            remembered,
-                            key=lambda t: map_obj.get_distance(
-                                hunter.get_cell(),
-                                find_cell_for_treasure(map_obj, t) or hunter.get_cell()
-                            )
+                elif known_treasures:
+                    closest = min(
+                        known_treasures,
+                        key=lambda t: map_obj.get_distance(
+                            hunter.get_cell(),
+                            find_cell_for_treasure(map_obj, t) or hunter.get_cell()
                         )
-                        closest_cell = find_cell_for_treasure(map_obj, closest)
-                        if closest_cell:
-                            next_step = min(
-                                neighbors,
-                                key=lambda c: map_obj.get_distance(c, closest_cell)
-                            )
-                            hunter.move_to(next_step)
-                        else:
-                            hunter.move_to(choice(neighbors))
+                    )
+                    closest_cell = find_cell_for_treasure(map_obj, closest)
+                    if closest_cell:
+                        next_step = min(
+                            neighbors,
+                            key=lambda c: map_obj.get_distance(c, closest_cell)
+                        )
+                        hunter.move_to(next_step)
                     else:
                         hunter.move_to(choice(neighbors))
+                else:
+                    hunter.move_to(choice(neighbors))
+
+            elif action == 2 and hunter.is_carrying_treasure():
+                for neighbor in neighbors:
+                    if any(obj.__class__.__name__ == "Hideout" for obj in neighbor.get_contents()):
+                        hunter.move_to(neighbor)
+                        for obj in neighbor.get_contents():
+                            if isinstance(obj, Hideout):
+                                obj.store_treasure(hunter)
+                        break
+                else:
+                    hunter.move_to(choice(neighbors))
+
+            elif action == 3:
+                knight_cells = [
+                    neighbor for neighbor in neighbors
+                    if any(obj.__class__.__name__ == "Knight" for obj in neighbor.get_contents())
+                ]
+                safe_cells = [n for n in neighbors if n not in knight_cells]
+                if safe_cells:
+                    hunter.move_to(choice(safe_cells))
+                else:
+                    hunter.move_to(choice(neighbors))
+
+            else:
+                hunter.move_to(choice(neighbors))
 
         hideout.share_knowledge()
         hideout.recruit_new_hunter()
